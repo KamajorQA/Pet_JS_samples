1fa2124e05637a28970fe7f8a65259c709a0876b
KamajorQA <KamajorQA@gmail.com>
2022-12-14 23:37:05 +0300

    Подключена библиотека p5, создан Canvas, отрисовано тело

---
+++ Zest_Canvas_Flappy_minigame/script.js
@@ -0,0 +1,11 @@
+function setup() {
+  createCanvas(800, 500);
+}
+
+function draw() {
+  background(153, 169, 205);
+  stroke(156, 29, 43);
+  strokeWeight(2);
+  fill(236, 237, 231);
+  ellipse(100, 100, 20 * 2);
+}
